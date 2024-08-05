5b15933116a7c2516e985a52f8a4502b6199f9bd
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-08-05 16:06:48 +0200

    Upd: JWT Addon to 1.5 from GitHub

--- authentication_server/addons/jwt/src/JWTVerifierBuilder.gd
+++ authentication_server/addons/jwt/src/JWTVerifierBuilder.gd
@@ -1,72 +1,84 @@
-extends JWTBaseBuilder
 class_name JWTVerifierBuilder
+extends JWTBaseBuilder
 
-var algorithm: JWTAlgorithm
-var claims: Dictionary = {}
-var leeway: int = 0
+var _algorithm: JWTAlgorithm
+var _claims: Dictionary = {}
+var _leeway: int = 0
 var _ignore_issued_at: bool = false
 
+
 func _init(algorithm: JWTAlgorithm):
-    self.algorithm = algorithm
+	self._algorithm = algorithm
+
 
 func add_claim(name: String, value) -> void:
-    match typeof(value):
-        TYPE_ARRAY, TYPE_PACKED_STRING_ARRAY: 
-            if value.size() == 0 : 
-                self.claims.erase(name)
-                return
-        TYPE_STRING: 
-            if value.length() == 0: 
-                self.claims.erase(name)
-                return
-        _:  
-            if value == null:
-                self.claims.erase(name)
-                return
-    self.claims[name] = value
+	match typeof(value):
+		TYPE_ARRAY, TYPE_PACKED_STRING_ARRAY:
+			if value.size() == 0:
+				self._claims.erase(name)
+				return
+		TYPE_STRING:
+			if value.length() == 0:
+				self._claims.erase(name)
+				return
+		_:
+			if value == null:
+				self._claims.erase(name)
+				return
+	self._claims[name] = value
+
 
 func with_any_of_issuers(issuers: PackedStringArray) -> JWTVerifierBuilder:
-    add_claim(JWTClaims.Public.ISSUER, issuers)
-    return self
+	add_claim(JWTClaims.Public.ISSUER, issuers)
+	return self
+
 
 func with_any_of_audience(audience: PackedStringArray) -> JWTVerifierBuilder:
-    add_claim(JWTClaims.Public.AUDIENCE, audience)
-    return self
+	add_claim(JWTClaims.Public.AUDIENCE, audience)
+	return self
+
 
 func accept_leeway(leeway: int) -> JWTVerifierBuilder:
-    self.leeway = leeway
-    return self
+	self._leeway = leeway
+	return self
+
 
 func accept_expire_at(leeway: int) -> JWTVerifierBuilder:
-    with_expires_at(leeway)
-    return self
+	with_expires_at(leeway)
+	return self
+
 
 func accept_not_before(leeway: int) -> JWTVerifierBuilder:
-    with_not_before(leeway)
-    return self
+	with_not_before(leeway)
+	return self
+
 
 func accept_issued_at(leeway: int) -> JWTVerifierBuilder:
-    with_issued_at(leeway)
-    return self
+	with_issued_at(leeway)
+	return self
+
 
 func ignore_issued_at(v: bool = true) -> JWTVerifierBuilder:
-    self._ignore_issued_at = true
-    return self
+	self._ignore_issued_at = v
+	return self
+
 
 func with_claim_presence(claim_name: String) -> JWTVerifierBuilder:
-    add_claim(claim_name, null)
-    return self
+	add_claim(claim_name, null)
+	return self
+
 
 func _add_leeway() -> void:
-    if (not claims.has(JWTClaims.Public.EXPRIES_AT)):
-        claims[JWTClaims.Public.EXPRIES_AT] = self.leeway
-    if (not claims.has(JWTClaims.Public.NOT_BEFORE)):
-        claims[JWTClaims.Public.NOT_BEFORE] = self.leeway
-    if (not claims.has(JWTClaims.Public.ISSUED_AT)):
-        claims[JWTClaims.Public.ISSUED_AT] = self.leeway
-    if (_ignore_issued_at):
-        claims.erase(JWTClaims.Public.ISSUED_AT)
-    
+	if not self._claims.has(JWTClaims.Public.EXPIRES_AT):
+		self._claims[JWTClaims.Public.EXPIRES_AT] = self._leeway
+	if not self._claims.has(JWTClaims.Public.NOT_BEFORE):
+		self._claims[JWTClaims.Public.NOT_BEFORE] = self._leeway
+	if not self._claims.has(JWTClaims.Public.ISSUED_AT):
+		self._claims[JWTClaims.Public.ISSUED_AT] = self._leeway
+	if self._ignore_issued_at:
+		self._claims.erase(JWTClaims.Public.ISSUED_AT)
+
+
 func build(clock: int = int(Time.get_unix_time_from_system())) -> JWTVerifier:
-    _add_leeway()
-    return JWTVerifier.new(self.algorithm, self.claims, clock)
+	_add_leeway()
+	return JWTVerifier.new(self._algorithm, self._claims, clock)
